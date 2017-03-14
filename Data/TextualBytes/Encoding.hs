e15821298d4935025753947ac99f2bd6ba147797
winterland1989 <winterland1989@gmail.com>
2017-03-14 19:23:30 +0800

    WIP

--- Data/TextualBytes/Encoding.hs
+++ Data/TextualBytes/Encoding.hs
@@ -46,112 +46,65 @@ data UTF8
 instance TextualEncoding UTF8 where
     {-# INLINE decodeChar #-}
     decodeChar _ addr len next idx =
-        case cnt of
-            0# ->
-                let !x1  = indexWord8OffAddr# addr idx
-                in (# chr# (word2Int# x1), 1# #)
+        case idx <# len of
             1# ->
                 let !x1  = indexWord8OffAddr# addr idx
-                    !x2 = indexWord8OffAddr# addr (idx +# 1#)
-                in if between x1 0xC2## 0xDF## && between x2 0x80## 0xBF##
-                    then (# chr2 x1 x2, 2# #)
-                    else (# '\0'#, 0# #)
-            2# ->
-                let !x1  = indexWord8OffAddr# addr idx
-                    !x2 = indexWord8OffAddr# addr (idx +# 1#)
-                    !x3 = indexWord8OffAddr# addr (idx +# 2#)
-                in if isTrue# (x1 `eqWord#` 0xE0##) &&
-                        between x2 0xA0## 0xBF## &&
-                        between x3 0x80## 0xBF##
-                    || between x1 0xE1## 0xEC## &&
-                        between x2 0x80## 0xBF## &&
-                        between x3 0x80## 0xBF##
-                    || isTrue# (x1 `eqWord#` 0xED##) &&
-                        between x2 0x80## 0x9F## &&
-                        between x3 0x80## 0xBF##
-                    || between x1 0xEE## 0xEF## &&
-                        between x2 0x80## 0xBF## &&
-                        between x3 0x80## 0xBF##
-                    then (# chr3 x1 x2 x3, 3# #)
-                    else (# '\0'#, 0# #)
-            3# ->
-                let !x1  = indexWord8OffAddr# addr idx
-                    !x2 = indexWord8OffAddr# addr (idx +# 1#)
-                    !x3 = indexWord8OffAddr# addr (idx +# 2#)
-                    !x4 = indexWord8OffAddr# addr (idx +# 3#)
-                in if isTrue# (x1 `eqWord#` 0xF0##) &&
-                        between x2 0x90## 0xBF## &&
-                        between x3 0x80## 0xBF## &&
-                        between x4 0x80## 0xBF##
-                    || between x1 0xF1## 0xF3## &&
-                        between x2 0x80## 0xBF## &&
-                        between x3 0x80## 0xBF## &&
-                        between x4 0x80## 0xBF##
-                    || isTrue# (x1 `eqWord#` 0xF4##) &&
-                        between x2 0x80## 0x8F## &&
-                        between x3 0x80## 0xBF## &&
-                        between x4 0x80## 0xBF##
-                    then (# chr4 x1 x2 x3 x4, 4# #)
-                    else (# '\0'#, 0# #)
-            0xEF# -> (# '\0'#, -1# #)
-            0xFF# -> (# '\0'#, 0# #)
-            --
-            -1# ->
-                let !x1  = indexWord8OffAddr# addr idx
-                    !x2 = indexWord8OffAddr# addr (idx +# 1#)
-                in if between x1 0xC2## 0xDF## && between x2 0x80## 0xBF##
-                    then (# chr2 x1 x2, 2# #)
-                    else (# '\0'#, 0# #)
-            -2# ->
-                let !x1  = indexWord8OffAddr# addr idx
-                    !x2 = indexWord8OffAddr# addr (idx +# 1#)
-                    !x3 = indexWord8OffAddr# addr (idx +# 2#)
-                in if isTrue# (x1 `eqWord#` 0xE0##) &&
-                        between x2 0xA0## 0xBF## &&
-                        between x3 0x80## 0xBF##
-                    || between x1 0xE1## 0xEC## &&
-                        between x2 0x80## 0xBF## &&
-                        between x3 0x80## 0xBF##
-                    || isTrue# (x1 `eqWord#` 0xED##) &&
-                        between x2 0x80## 0x9F## &&
-                        between x3 0x80## 0xBF##
-                    || between x1 0xEE## 0xEF## &&
-                        between x2 0x80## 0xBF## &&
-                        between x3 0x80## 0xBF##
-                    then (# chr3 x1 x2 x3, 3# #)
-                    else (# '\0'#, 0# #)
-            -3# ->
-                let !x1  = indexWord8OffAddr# addr idx
-                    !x2 = indexWord8OffAddr# addr (idx +# 1#)
-                    !x3 = indexWord8OffAddr# addr (idx +# 2#)
-                    !x4 = indexWord8OffAddr# addr (idx +# 3#)
-                in if isTrue# (x1 `eqWord#` 0xF0##) &&
-                        between x2 0x90## 0xBF## &&
-                        between x3 0x80## 0xBF## &&
-                        between x4 0x80## 0xBF##
-                    || between x1 0xF1## 0xF3## &&
-                        between x2 0x80## 0xBF## &&
-                        between x3 0x80## 0xBF## &&
-                        between x4 0x80## 0xBF##
-                    || isTrue# (x1 `eqWord#` 0xF4##) &&
-                        between x2 0x80## 0x8F## &&
-                        between x3 0x80## 0xBF## &&
-                        between x4 0x80## 0xBF##
-                    then (# chr4 x1 x2 x3 x4, 4# #)
-                    else (# '\0'#, 0# #)
+                    !c = indexWord8OffAddr# utf8HeadTable# (word2Int# x1)
+                in case c of
+                    0xFF## -> (# '\0'#, 0# #)
+                    _ ->
+                        let !cnt = case (word2Int# c +# idx) <# len of
+                                1# -> word2Int# c
+                                0# -> case next `eqAddr#` nullAddr# of
+                                    0# -> 0xEF#
+                                    1# -> len -# idx -# (word2Int# c)
+                        in case cnt of
+                            0# -> (# chr# (word2Int# x1), 1# #)
+                            1# ->
+                                let !x2 = indexWord8OffAddr# addr (idx +# 1#)
+                                in if between x1 0xC2## 0xDF## && between x2 0x80## 0xBF##
+                                    then (# chr2 x1 x2, 2# #)
+                                    else (# '\0'#, 0# #)
+                            2# ->
+                                let !x2 = indexWord8OffAddr# addr (idx +# 1#)
+                                    !x3 = indexWord8OffAddr# addr (idx +# 2#)
+                                in if isTrue# (x1 `eqWord#` 0xE0##) &&
+                                        between x2 0xA0## 0xBF## &&
+                                        between x3 0x80## 0xBF##
+                                    || between x1 0xE1## 0xEC## &&
+                                        between x2 0x80## 0xBF## &&
+                                        between x3 0x80## 0xBF##
+                                    || isTrue# (x1 `eqWord#` 0xED##) &&
+                                        between x2 0x80## 0x9F## &&
+                                        between x3 0x80## 0xBF##
+                                    || between x1 0xEE## 0xEF## &&
+                                        between x2 0x80## 0xBF## &&
+                                        between x3 0x80## 0xBF##
+                                    then (# chr3 x1 x2 x3, 3# #)
+                                    else (# '\0'#, 0# #)
+                            3# ->
+                                let !x2 = indexWord8OffAddr# addr (idx +# 1#)
+                                    !x3 = indexWord8OffAddr# addr (idx +# 2#)
+                                    !x4 = indexWord8OffAddr# addr (idx +# 3#)
+                                in if isTrue# (x1 `eqWord#` 0xF0##) &&
+                                        between x2 0x90## 0xBF## &&
+                                        between x3 0x80## 0xBF## &&
+                                        between x4 0x80## 0xBF##
+                                    || between x1 0xF1## 0xF3## &&
+                                        between x2 0x80## 0xBF## &&
+                                        between x3 0x80## 0xBF## &&
+                                        between x4 0x80## 0xBF##
+                                    || isTrue# (x1 `eqWord#` 0xF4##) &&
+                                        between x2 0x80## 0x8F## &&
+                                        between x3 0x80## 0xBF## &&
+                                        between x4 0x80## 0xBF##
+                                    then (# chr4 x1 x2 x3 x4, 4# #)
+                                    else (# '\0'#, 0# #)
+
+            0# -> (# '\0'#, -1# #)
 
           where
             !(Table utf8HeadTable#) = utf8HeadTable
-            !cnt = case idx <# len of
-                1# ->
-                    let !x1  = indexWord8OffAddr# addr idx
-                        !c = indexInt8OffAddr# utf8HeadTable# (word2Int# x1)
-                    in case (c +# idx) <# len of
-                        1# -> c
-                        0# -> case next `eqAddr#` nullAddr# of
-                            0# -> 0xEF#
-                            1# -> len -# idx -# c
-                0# -> 0xEF#
 
             chr2 x1 x2 = chr# (word2Int# (
                 or# (uncheckedShiftL# (and# x1 0x1f##) 6#)
