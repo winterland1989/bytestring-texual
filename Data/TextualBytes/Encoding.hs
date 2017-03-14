63c306eed5d567226c32e5e650be0939fc31ff58
winterland1989 <winterland1989@gmail.com>
2017-03-14 20:18:46 +0800

    inlineable

--- Data/TextualBytes/Encoding.hs
+++ Data/TextualBytes/Encoding.hs
@@ -44,86 +44,120 @@ instance TextualEncoding ASCII where
 
 data UTF8
 instance TextualEncoding UTF8 where
-    {-# INLINE decodeChar #-}
+    {-# INLINEABLE decodeChar #-}
     decodeChar _ addr len next idx =
+        -- bound check
         case idx <# len of
+            0# -> (# '\0'#, -1# #)
             1# ->
                 let !x1  = indexWord8OffAddr# addr idx
-                    !c = indexWord8OffAddr# utf8HeadTable# (word2Int# x1)
+                    !(Table utf8HeadTable#) = utf8HeadTable
+                    !c = indexInt8OffAddr# utf8HeadTable# (word2Int# x1)
                 in case c of
-                    0xFF## -> (# '\0'#, 0# #)
+                    0xFF# -> (# '\0'#, 0# #)
                     _ ->
-                        let !cnt = case (word2Int# c +# idx) <# len of
-                                1# -> word2Int# c
+                        -- next bound check
+                        let !cnt = case (c +# idx) <# len of
+                                1# -> c
                                 0# -> case next `eqAddr#` nullAddr# of
-                                    0# -> 0xEF#
-                                    1# -> len -# idx -# (word2Int# c)
+                                    0# -> 0xFF#
+                                    1# -> len -# idx -# c
                         in case cnt of
                             0# -> (# chr# (word2Int# x1), 1# #)
                             1# ->
                                 let !x2 = indexWord8OffAddr# addr (idx +# 1#)
-                                in if between x1 0xC2## 0xDF## && between x2 0x80## 0xBF##
-                                    then (# chr2 x1 x2, 2# #)
+                                in if utf8_validate2 x1 x2
+                                    then (# utf8_chr2 x1 x2, 2# #)
                                     else (# '\0'#, 0# #)
                             2# ->
                                 let !x2 = indexWord8OffAddr# addr (idx +# 1#)
                                     !x3 = indexWord8OffAddr# addr (idx +# 2#)
-                                in if isTrue# (x1 `eqWord#` 0xE0##) &&
-                                        between x2 0xA0## 0xBF## &&
-                                        between x3 0x80## 0xBF##
-                                    || between x1 0xE1## 0xEC## &&
-                                        between x2 0x80## 0xBF## &&
-                                        between x3 0x80## 0xBF##
-                                    || isTrue# (x1 `eqWord#` 0xED##) &&
-                                        between x2 0x80## 0x9F## &&
-                                        between x3 0x80## 0xBF##
-                                    || between x1 0xEE## 0xEF## &&
-                                        between x2 0x80## 0xBF## &&
-                                        between x3 0x80## 0xBF##
-                                    then (# chr3 x1 x2 x3, 3# #)
+                                in if utf8_validate3 x1 x2 x3
+                                    then (# utf8_chr3 x1 x2 x3, 3# #)
                                     else (# '\0'#, 0# #)
                             3# ->
                                 let !x2 = indexWord8OffAddr# addr (idx +# 1#)
                                     !x3 = indexWord8OffAddr# addr (idx +# 2#)
                                     !x4 = indexWord8OffAddr# addr (idx +# 3#)
-                                in if isTrue# (x1 `eqWord#` 0xF0##) &&
-                                        between x2 0x90## 0xBF## &&
-                                        between x3 0x80## 0xBF## &&
-                                        between x4 0x80## 0xBF##
-                                    || between x1 0xF1## 0xF3## &&
-                                        between x2 0x80## 0xBF## &&
-                                        between x3 0x80## 0xBF## &&
-                                        between x4 0x80## 0xBF##
-                                    || isTrue# (x1 `eqWord#` 0xF4##) &&
-                                        between x2 0x80## 0x8F## &&
-                                        between x3 0x80## 0xBF## &&
-                                        between x4 0x80## 0xBF##
-                                    then (# chr4 x1 x2 x3 x4, 4# #)
+                                in if utf8_validate4 x1 x2 x3 x4
+                                    then (# utf8_chr4 x1 x2 x3 x4, 4# #)
+                                    else (# '\0'#, 0# #)
+                            0# ->
+                                let !x2 = indexWord8OffAddr# next 0#
+                                in if utf8_validate2 x1 x2
+                                    then (# utf8_chr2 x1 x2, 2# #)
+                                    else (# '\0'#, 0# #)
+                            -1# ->
+                                let !x2 = indexWord8OffAddr# next 1#
+                                    !x3 = indexWord8OffAddr# next 2#
+                                in if utf8_validate3 x1 x2 x3
+                                    then (# utf8_chr3 x1 x2 x3, 3# #)
+                                    else (# '\0'#, 0# #)
+                            -2# ->
+                                let !x2 = indexWord8OffAddr# next 0#
+                                    !x3 = indexWord8OffAddr# next 1#
+                                    !x4 = indexWord8OffAddr# next 2#
+                                in if utf8_validate4 x1 x2 x3 x4
+                                    then (# utf8_chr4 x1 x2 x3 x4, 4# #)
                                     else (# '\0'#, 0# #)
 
-            0# -> (# '\0'#, -1# #)
+                            0xFF# -> (# '\0'#, -2# #)
 
-          where
-            !(Table utf8HeadTable#) = utf8HeadTable
-
-            chr2 x1 x2 = chr# (word2Int# (
-                or# (uncheckedShiftL# (and# x1 0x1f##) 6#)
-                    (and# x2 0x3f##)
-                ))
-            chr3 x1 x2 x3 = chr# (word2Int# (
-                or# (uncheckedShiftL# (and# x1 0xf##) 12#)
-                    (or# (uncheckedShiftL# (and# x2 0x3f##) 6#)
-                         (and# x3 0x3f##))
-                ))
-            chr4 x1 x2 x3 x4 = chr# (word2Int# (
-                or# (uncheckedShiftL# (and# x1 0x7##) 18#)
-                    (or# (uncheckedShiftL# (and# x2 0x3f##) 12#)
-                        (or# (uncheckedShiftL# (and# x3 0x3f##) 6#)
-                            (and# x4 0x3f##)))
-                ))
-
-    {-# INLINE encodeChar #-}
+    {-# INLINEABLE encodeChar #-}
     encodeChar _ = charUtf8
+
+{-# INLINE utf8_validate2 #-}
+utf8_validate2 x1 x2 = (between x1 0xC2## 0xDF##) && (between x2 0x80## 0xBF##)
+
+{-# INLINE utf8_validate3 #-}
+utf8_validate3 x1 x2 x3 =
+    isTrue# (x1 `eqWord#` 0xE0##) &&
+        between x2 0xA0## 0xBF## &&
+        between x3 0x80## 0xBF##
+    || between x1 0xE1## 0xEC## &&
+        between x2 0x80## 0xBF## &&
+        between x3 0x80## 0xBF##
+    || isTrue# (x1 `eqWord#` 0xED##) &&
+        between x2 0x80## 0x9F## &&
+        between x3 0x80## 0xBF##
+    || between x1 0xEE## 0xEF## &&
+        between x2 0x80## 0xBF## &&
+        between x3 0x80## 0xBF##
+
+{-# INLINE utf8_validate4 #-}
+utf8_validate4 x1 x2 x3 x4 =
+    isTrue# (x1 `eqWord#` 0xF0##) &&
+        between x2 0x90## 0xBF## &&
+        between x3 0x80## 0xBF## &&
+        between x4 0x80## 0xBF##
+    || between x1 0xF1## 0xF3## &&
+        between x2 0x80## 0xBF## &&
+        between x3 0x80## 0xBF## &&
+        between x4 0x80## 0xBF##
+    || isTrue# (x1 `eqWord#` 0xF4##) &&
+        between x2 0x80## 0x8F## &&
+        between x3 0x80## 0xBF## &&
+        between x4 0x80## 0xBF##
+
+{-# INLINE utf8_chr2 #-}
+utf8_chr2 x1 x2 = chr# (word2Int# (
+    or# (uncheckedShiftL# (and# x1 0x1f##) 6#)
+        (and# x2 0x3f##)
+    ))
+
+{-# INLINE utf8_chr3 #-}
+utf8_chr3 x1 x2 x3 = chr# (word2Int# (
+    or# (uncheckedShiftL# (and# x1 0xf##) 12#)
+        (or# (uncheckedShiftL# (and# x2 0x3f##) 6#)
+             (and# x3 0x3f##))
+    ))
+{-# INLINE utf8_chr4 #-}
+utf8_chr4 x1 x2 x3 x4 = chr# (word2Int# (
+    or# (uncheckedShiftL# (and# x1 0x7##) 18#)
+        (or# (uncheckedShiftL# (and# x2 0x3f##) 12#)
+            (or# (uncheckedShiftL# (and# x3 0x3f##) 6#)
+                (and# x4 0x3f##)))
+    ))
 
 data Table = Table Addr#
 utf8HeadTable :: Table
